c3f812370efed215803815d4ece7e53753fe5d2e
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-09-24 13:09:37 +0100

    swap hard coded info for props

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Ilayda"
+        timeAgo="Today 03:23AM"
+        avatar={faker.image.avatar()}
+        content="Yo this is sick!"
+      />
+      <CommentDetail
+        author="Dilara"
+        timeAgo="Today 05:29AM"
+        avatar={faker.image.avatar()}
+        content="Mmmmm I like this"
+      />
+      <CommentDetail
+        author="Muz"
+        timeAgo="Yesterday 11:05PM"
+        avatar={faker.image.avatar()}
+        content="I'm ginger"
+      />
     </div>
   );
 };
 
-ReactDOM.render(<App />,
-  document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
