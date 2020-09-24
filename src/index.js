4ce7f1d0a8f918ce8f39bdf277a04a3ad0b1fed6
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-09-24 22:29:27 +0100

    Add approval card with buttons to user comments

--- src/index.js
+++ src/index.js
@@ -2,28 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Ilayda"
-        timeAgo="Today 03:23AM"
-        avatar={faker.image.avatar()}
-        content="Yo this is sick!"
-      />
-      <CommentDetail
-        author="Dilara"
-        timeAgo="Today 05:29AM"
-        avatar={faker.image.avatar()}
-        content="Mmmmm I like this"
-      />
-      <CommentDetail
-        author="Muz"
-        timeAgo="Yesterday 11:05PM"
-        avatar={faker.image.avatar()}
-        content="I'm ginger"
-      />
+      <ApprovalCard>
+      <div>
+        <h4>Warning!</h4>
+        Are you sure you wanna do this homie?
+      </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Ilayda"
+          timeAgo="Today 03:23AM"
+          avatar={faker.image.avatar()}
+          content="Yo this is sick!"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Dilara"
+          timeAgo="Today 05:29AM"
+          avatar={faker.image.avatar()}
+          content="Mmmmm I like this"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Muz"
+          timeAgo="Yesterday 11:05PM"
+          avatar={faker.image.avatar()}
+          content="I'm ginger"/>
+      </ApprovalCard>
     </div>
   );
 };
